39c408880d761d387ec262b2f938744f4202e6f5
thePeras <jtopereira@gmail.com>
2022-05-24 22:58:22 +0100

    villate.org website

--- main.js
+++ main.js
@@ -114,3 +114,62 @@ if(window.location.hostname == "www.w3resource.com" ||
    window.location.hostname == "w3resource.com"){
     document.querySelectorAll(".toolbar").forEach(el => el.style.display = "none")
 }
+
+//villate.org
+const splitArray = (myArray, tag = "BR") => {
+    let start_i = 0;
+    let result = [];
+    myArray.forEach((element, i) => {
+        if(element.tagName == tag){
+            result.push(myArray.slice(start_i, i))
+            start_i = i+1
+        }
+    })
+    result.push(myArray.slice(start_i, myArray.length))
+    return result
+}
+const editArray = (myArray) => {
+    let result = []
+    for(let element of myArray){
+        if(element[0].classList.contains('maxm')){
+            result.push({
+                text: element.slice(1).map(el => el.innerText).join(''),
+                last: element.pop()
+            })
+        }
+    }
+    return result
+}
+
+document.querySelectorAll('.maxima').forEach(div => {
+    const lines = editArray( splitArray([...div.children], "BR") );
+    for(let line of lines){
+        console.log(line.text)
+
+        //ICON
+        const img = document.createElement("img");
+        img.src = "https://img.icons8.com/external-aficons-studio-flat-aficons-studio/344/external-copy-user-interface-aficons-studio-flat-aficons-studio.png";
+        img.style = "height: 80%; margin: auto; border:none";
+
+        //BUTTON
+        let copyButton = document.createElement("div");
+        copyButton.className = "copy-button";
+        copyButton.type = "button";
+        copyButton.appendChild(img);
+
+        //COPY FUNCTION
+        copyButton.onclick = () => {
+            //coping to clipboard
+            if (navigator && navigator.clipboard && navigator.clipboard.writeText)
+            return navigator.clipboard.writeText(line.text);
+            return Promise.reject('The Clipboard API is not available.');
+        }
+
+        copyButton.style.position = "relative";
+        copyButton.style.float = "right";
+        copyButton.style.width = "23px";
+        copyButton.style.height = "23px";
+
+        line.last.append(copyButton)
+    }
+})
